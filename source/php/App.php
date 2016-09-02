1c66aa5e7ef9fad2755d243089a85376d0d4b8cc
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-02 10:57:11 +0200

    Enqueue js

--- source/php/App.php
+++ source/php/App.php
@@ -6,6 +6,20 @@ class App
 {
     public function __construct()
     {
+        // add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
+
         new Revision();
+        new Swapper();
+    }
+
+    public function enqueueScripts()
+    {
+        if (!is_admin() || !get_post_type() === 'attachment') {
+            return;
+        }
+
+        wp_enqueue_media();
+        wp_enqueue_script('attachment-revisions', ATTACHMENTREVISIONS_URL . '/dist/js/attachment-revisions.min.js', null, '1.0.0');
     }
 }

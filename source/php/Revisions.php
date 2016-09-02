8d251c4f2d099347e345385649497c718bd66c67
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-09-02 09:40:41 +0200

    Added some comments

--- source/php/Revisions.php
+++ source/php/Revisions.php
@@ -10,6 +10,10 @@ class Revisions
         add_action('attachment_updated', array($this, 'addRevision'), 10, 3);
     }
 
+    /**
+     * Add revision support to the attachment post type
+     * @return void
+     */
     public function activate()
     {
         add_post_type_support('attachment', 'revisions');
@@ -28,18 +32,20 @@ class Revisions
         $afterComp = clone $after;
         $beforeComp = clone $before;
 
+        // Unset post_modified and post_modified_gmt since it changes all the time
+        // which makes objects uncomparable
         unset($afterComp->post_modified);
         unset($afterComp->post_modified_gmt);
         unset($beforeComp->post_modified);
         unset($beforeComp->post_modified_gmt);
 
-        // No fields changed
+        // Compares after and before, if no changes made return false
         if ($afterComp == $beforeComp) {
             return false;
         }
 
         // Something changed, create a revision
-        // Todo: Check if we can use non-private function for creating the revision
+        // @todo Check if we can use non-private function for creating the revision
         $revision = _wp_put_post_revision($before);
         return $revision;
     }

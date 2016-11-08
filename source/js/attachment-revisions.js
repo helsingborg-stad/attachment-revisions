533204e1bb65c53ffed5f449463cb4c2b7fc6574
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-08 09:04:59 +0100

    Class orientaded js

--- source/js/attachment-revisions.js
+++ source/js/attachment-revisions.js
@@ -1,77 +1,98 @@
 var AttatchmentRevisions = {};
+var AttachmentRevisions = AttachmentRevisions || {};
+AttachmentRevisions.MediaUpload = (function ($) {
 
-jQuery(document).ready(function ($) {
+    var _fileUploader = null;
 
-    var mediaselector = null;
+    function MediaUpload() {
+        this.handleEvents();
+    }
 
-    // Replace
-    $(document).on('click', '[data-action="media-replacer-replace"]', function () {
+    MediaUpload.prototype.handleEvents = function() {
+        // Replace media button
+        $(document).on('click', '[data-action="media-replacer-replace"]', function () {
+            this.openFileUploader();
+        }.bind(this));
 
-        if ($(this).parents('.media-modal').length) {
-            location.href = $(this).data('edit-link');
-            return;
-        }
-
-        var mimeType = $(this).data('mime');
-        var mimeParts = mimeType.split('/');
-
-        if (!mediaselector) {
-            mediaselector = wp.media({
-                title: 'Select replacement file',
-                button: {
-                    text: 'Replace'
-                },
-                multiple: false,
-                library: {
-                    type: mimeParts[0],
-                    subtype: mimeParts[1]
-                }
-            });
-
-            // On mediaselector select
-            mediaselector.on('select', function () {
-                var selected = mediaselector.state().get('selection').first().toJSON().id;
-
-                if (typeof selected != 'undefined') {
-                    $('[name="media-replacer-replace-with"]').val(selected).closest('form').submit();
-                } else {
-                    alert('You did not select a file. Media will not be replaced.');
-                }
-            });
-        }
-
-        // Open the media selector
-        var mediaselectorElement = $(mediaselector.open().el);
-        $('.media-router a:first-of-type').trigger('click');
-
-        $(document).on('click', '.media-router > a', function () {
-            $('.attachment-preview:not(.type-' + mimeParts[0] + '), .attachment-preview:not(.subtype-' + mimeParts[1] + ')')
-                    .parent('li').remove();
+        // Revisions button
+        $(document).on('click', '[data-action="media-replacer-revisions"]', function () {
+            if ($(this).parents('.media-modal').length) {
+                location.href = $(this).data('edit-link');
+                return false;
+            }
         });
-    });
 
-    $(document).on('click', '[data-action="media-replacer-revisions"]', function () {
+        $(document).on('click', '.media-replace-revisions [data-restore]', function () {
+            if ($(this).hasClass('selected')) {
+                $('[name="media-replace-restore"]').val('');
+                $(this).removeClass('selected');
+                return;
+            }
+
+            $('.media-replace-revisions li.selected').removeClass('selected');
+
+            var path = $(this).data('restore');
+            $('[name="media-replace-restore"]').val(path);
+            $(this).addClass('selected');
+            return;
+        });
+    };
+
+    MediaUpload.prototype.openFileUploader = function() {
+        // If opened from media modal redirect to edit post page
         if ($(this).parents('.media-modal').length) {
             location.href = $(this).data('edit-link');
-            return false;
-        }
-    });
-
-    // Revision
-    $(document).on('click', '.media-replace-revisions [data-restore]', function () {
-        if ($(this).hasClass('selected')) {
-            $('[name="media-replace-restore"]').val('');
-            $(this).removeClass('selected');
             return;
         }
 
-        $('.media-replace-revisions li.selected').removeClass('selected');
+        if (_fileUploader) {
+            // Open the modal
+            _fileUploader.open();
 
-        var path = $(this).data('restore');
-        $('[name="media-replace-restore"]').val(path);
-        $(this).addClass('selected');
-        return;
-    });
+            // Default to upload file tab
+            $('.media-router a:first-of-type').trigger('click');
 
-});
+            return;
+        }
+
+        this.setupFileUploader();
+    };
+
+    MediaUpload.prototype.setupFileUploader = function() {
+        var query = wp.media.query({
+            orderby: 'date',
+            query: true,
+            uploadedTo: -1
+        });
+
+        _fileUploader = wp.media({
+            title: 'Select replacement file',
+            button: {
+                text: 'Replace'
+            },
+            multiple: false,
+            states: [
+                new wp.media.controller.Library({
+                    library: query
+                })
+            ]
+        });
+
+        _fileUploader.on('select', function () {
+            var selected = _fileUploader.state().get('selection').first().toJSON().id;
+
+            if (typeof selected != 'undefined') {
+                $('[name="media-replacer-replace-with"]').val(selected).closest('form').submit();
+            } else {
+                alert('You did not select a file. Media will not be replaced.');
+            }
+        });
+
+        this.openFileUploader();
+    };
+
+
+    return new MediaUpload();
+
+})(jQuery);
 

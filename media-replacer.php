a571145c874a6f5cb278e4213224bc5888daada4
GitHub Action <>
2026-09-09 11:28:56 +0000

    build: update package file versions to 3.0.13

--- media-replacer.php
+++ media-replacer.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Attatchment Revisions
  * Plugin URI:
  * Description:       Replace media files and keep revisions
- * Version: 3.0.11
+ * Version: 3.0.13
  * Author:            Kristoffer Svanmark
  * Author URI:
  * License:           MIT
